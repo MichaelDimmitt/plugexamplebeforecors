fb52872636b654aa0f219165af57d2c17ba933f7
michaeldimmitt <michaelgdimmitt@gmail.com>
2018-07-25 16:04:04 -0400

    boiler plate

---
+++ lib/plugexamplebeforecors.ex
@@ -0,0 +1,18 @@
+defmodule Plugexamplebeforecors do
+  @moduledoc """
+  Documentation for Plugexamplebeforecors.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Plugexamplebeforecors.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/plugexamplebeforecors_test.exs
@@ -0,0 +1,8 @@
+defmodule PlugexamplebeforecorsTest do
+  use ExUnit.Case
+  doctest Plugexamplebeforecors
+
+  test "greets the world" do
+    assert Plugexamplebeforecors.hello() == :world
+  end
+end
